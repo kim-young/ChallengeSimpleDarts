3e97c34032a1975d3c2b1a28ef6e36a2aebd0bcd
Young Kim <younghyun87@gmail.com>
2018-03-12 14:35:29 -0400

    C# Challenge #11

---
+++ ChallengeSimpleDarts/Score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Darts;
+
+namespace ChallengeSimpleDarts
+{
+    public class Score
+    {
+        public static void CalculateScore(Player player, Dart dart)
+        {
+            int score = 0;
+
+            if (dart.OuterBand) score = dart.Point * 2;
+            else if (dart.InnerBand) score = dart.Point * 3;
+            else score = dart.Point;
+
+            if (dart.InnerBand && dart.Point == 0) score += 50;
+            else if(dart.Point == 0) score += 25;
+
+            player.Score += score;  
+        }
+    }
+}
---
+++ Darts/Dart.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darts
+{
+    public class Dart
+    {
+
+        private Random _random;
+        public int Point { get; set; }
+        public bool OuterBand { get; set; }
+        public bool InnerBand { get; set; }
+        public int Multiplier { get; set; }
+
+        public Dart(Random random)
+        {
+            _random = random;
+        }
+
+        public void Throw()
+        {
+            this.Point = _random.Next(0, 21);
+
+            this.Multiplier = _random.Next(1, 101);
+            if (Multiplier > 95) OuterBand = true;
+            else if (Multiplier > 90) InnerBand = true;
+        }
+    }
+}
